f1c4d976b76d77d148ff4d4266f76d98eba9e247
Istvan Petrovan <petrovanistvan@gmail.com>
2023-04-08 21:50:01 +0200

    added task object

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-// import { v4 as uuidV4 } from 'uuid';
-
-// console.log(uuidV4());
+import { v4 as uuidV4 } from 'uuid';
 
 const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.getElementById(
@@ -8,8 +6,15 @@ const form = document.getElementById(
 ) as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
-form?.addEventListener('submit', () => {
+form?.addEventListener('submit', (e) => {
   e.preventDefault();
 
   if (input?.value == '' || input?.value == null) return;
+
+  const task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
 });
