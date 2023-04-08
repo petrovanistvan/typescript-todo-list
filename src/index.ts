7d60dbebbba26e8c9c7f6a8a4dc7d3df37c2e0ec
Istvan Petrovan <petrovanistvan@gmail.com>
2023-04-08 21:15:42 +0200

    added selectors with type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-import { v4 as uuidV4 } from 'uuid';
+// import { v4 as uuidV4 } from 'uuid';
 
-console.log(uuidV4());
+// console.log(uuidV4());
+
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector<HTMLFormElement>('#new-task-form');
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
