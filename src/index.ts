f47ed280fbe393cd341e7582a8bfba80eedb76cc
Istvan Petrovan <petrovanistvan@gmail.com>
2023-04-08 21:20:21 +0200

    optional chaining

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,13 @@
 // console.log(uuidV4());
 
 const list = document.querySelector<HTMLUListElement>('#list');
-const form = document.querySelector<HTMLFormElement>('#new-task-form');
+const form = document.getElementById(
+  '#new-task-form',
+) as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+form?.addEventListener('submit', () => {
+  e.preventDefault();
+
+  if (input?.value == '' || input?.value == null) return;
+});
